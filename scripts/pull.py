0adb4a20def7b06cc832ad1b1c208538016553f7
aoisensi <me@aoisensi.info>
2020-09-12 22:34:05 +0900

    fix script

--- scripts/pull.py
+++ scripts/pull.py
@@ -9,28 +9,38 @@ import pathlib
 import json
 
 base_url = 'https://raw.githubusercontent.com/dotabuff/d2vpkr/master/dota/resource/localization/'
+
+def default(data):
+    return data['lang']['Tokens']
+
+def simple(key):
+    return lambda data: data[key]
+
 files = [
-    'abilities',
-    'broadcastfacts',
-    'chat',
-    'dota',
-    'gameui',
-#    'hero_chat_wheel', TODO: support later
-    'hero_lore',
-    'items',
-#    'leagues', TODO: support later
-    'richpresence',
-#    'patchnotes/patchnotes', TODO: support later
+    ('abilities', default),
+    ('broadcastfacts', default),
+    ('chat', default),
+    ('dota', default),
+    ('gameui', default),
+    ('hero_chat_wheel', simple('hero_chat_wheel')),
+    ('hero_lore', default),
+#   huge
+#   ('items', default),
+    ('leagues', simple('leagues')),
+    ('richpresence', default),
+#   useless?
+#   ('patchnotes/patchnotes', simple('patch')),
 ]
 
 os.makedirs('localization/patchnotes', exist_ok=True)
 
 # ダウンロード
 for file in files:
-    print(file)
-    url = base_url + file + '_english' + '.txt'
+    print(file[0])
+    url = base_url + file[0] + '_english' + '.txt'
     req = urllib.request.Request(url)
     with urllib.request.urlopen(req) as res:
-        tokens = vdf.loads(res.read().decode())['lang']['Tokens']
-        with open('localization/' + file + '_english' + '.json', 'w') as fw:
+        data = vdf.loads(res.read().decode())
+        tokens = file[1](data)
+        with open('localization/' + file[0] + '_english' + '.json', 'w') as fw:
             json.dump(tokens, fw, indent=4)
